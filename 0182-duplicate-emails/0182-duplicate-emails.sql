e36e26590eb0ce6e0d7891f4a7aa7134c31e1e20
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-07-31 22:41:17 +0300

    Time: 389 ms (30.06%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,19 @@
 # Write your MySQL query statement below
+/*
 Select 
 Email from 
 Person 
 group by Email 
 having count(*) > 1;
+
+*/
+
+Select distinct Email 
+from Person
+where Email In (
+    Select Email
+    from Person 
+    Group by Email
+    having Count(*) > 1
+) ;
 
