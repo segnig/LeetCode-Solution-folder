0252529376588909ef105c879a7740af67acbc1e
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-08-02 14:03:43 +0300

    Time: 470 ms (59.28%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,30 +1,26 @@
 -- Write your PostgreSQL query statement below  
-WITH StudentSubjects AS (  
-    SELECT  
-        s.student_id,  
-        s.student_name,  
-        sub.subject_name  
-    FROM  
-        Students s  
-    CROSS JOIN  
-        Subjects sub  
-)  
-SELECT  
-    ss.student_id,  
-    ss.student_name,  
-    ss.subject_name,  
-    COALESCE(COUNT(e.student_id), 0) AS attended_exams  
-FROM  
-    StudentSubjects ss  
-LEFT JOIN  
-    Examinations e  
-ON  
-    ss.student_id = e.student_id  
-    AND ss.subject_name = e.subject_name  
-GROUP BY  
-    ss.student_id,  
-    ss.student_name,  
-    ss.subject_name  
-ORDER BY  
-    ss.student_id,  
-    ss.subject_name;
+with ss as(
+    select
+    s.student_id,
+    s.student_name,
+    su.subject_name
+    from Students s
+    Cross Join 
+        subjects su
+)
+select 
+    ss.student_id,
+    ss.student_name,
+    ss.subject_name,
+    Coalesce(count(e.subject_name), 0) as attended_exams
+    from ss
+    Left join Examinations e
+    on e.student_id = ss.student_id and 
+        e.subject_name = ss.subject_name
+    group by 
+        ss.student_id,
+        ss.student_name,
+        ss.subject_name
+    order by
+        ss.student_id,
+        ss.subject_name
