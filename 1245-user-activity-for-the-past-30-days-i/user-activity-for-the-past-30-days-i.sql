607e5e64a424016219ce5b90d28f8ba2f49b9306
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-08-03 11:05:45 +0300

    Time: 895 ms (5.46%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,7 +1,7 @@
 -- Write your PostgreSQL query statement below
 Select 
     activity_date as day,
-    count(distinct user_id) active_users
+    count(distinct user_id) as active_users
 from Activity
     where 
         activity_date <= date('2019-07-27') and activity_date > date('2019-07-27') - 30
