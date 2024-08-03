a188efe2ba635b956cef559f96db613e144813aa
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-08-03 10:36:40 +0300

    Time: 531 ms (89.53%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,2 +1,7 @@
-# Write your MySQL query statement below
-select teacher_id, count(distinct subject_id) as cnt from teacher group by teacher_id;
+-- Write your PostgreSQL query statement below
+select teacher_id, count(Distinct subject_id) cnt
+from Teacher
+    where (Subject_id, dept_id) IN (
+        Select subject_id, dept_id from Teacher group by Subject_id, dept_id having count(teacher_id) = 1
+    )
+group by teacher_id
