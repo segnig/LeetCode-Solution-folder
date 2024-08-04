0d3df97144fdd4bfe9f8e8d837327454e0b0d887
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-08-04 19:41:56 +0300

    Time: 376 ms (19.59%), Space: 0B (100%) - LeetHub

---
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+Select
+    employee_id 
+from Employees
+where manager_id not in
+(
+    Select employee_id 
+    from Employees
+)
+and salary < 30000
+order by employee_id;
