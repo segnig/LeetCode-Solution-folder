c7469ad242ac43fd531313f68fd3cccfa834e881
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-10-15 09:31:27 +0300

    Time: 380 ms (6.98%), Space: 16.6 MB (32.56%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumSteps(self, s):
+        placed, result = 0, 0
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "1":
+                print("here")
+                result += len(s) - i - placed - 1
+                placed += 1
+        return result
+
+        
