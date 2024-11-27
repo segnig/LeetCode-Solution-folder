bd911ad8851c2ddbb02fbe9f9a4bf47a21a5d56e
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-11-27 03:00:52 -0800

    Time: 46 ms (75.09%) | Memory: 17.2 MB (12.92%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
@@ -0,0 +1,21 @@
+from typing import List  
+
+class Solution:  
+    def countValidSelections(self, nums: List[int]) -> int:  
+        total_sum = sum(nums)  
+        prefix_sum = [0]  
+
+        for n in nums:  
+            prefix_sum.append(prefix_sum[-1] + n)  
+
+        result = 0  
+
+        # Iterate through the prefix sums  
+        for n in range(1, len(prefix_sum)):  
+            if prefix_sum[-1] - 2 * prefix_sum[n] == 0 and nums[n - 1] == 0:  
+                result += 2   
+            elif (prefix_sum[-1] - prefix_sum[n] == prefix_sum[n] - 1 or   
+                  prefix_sum[-1] - prefix_sum[n] == prefix_sum[n] + 1) and nums[n - 1] == 0:  
+                result += 1  
+        
+        return result
