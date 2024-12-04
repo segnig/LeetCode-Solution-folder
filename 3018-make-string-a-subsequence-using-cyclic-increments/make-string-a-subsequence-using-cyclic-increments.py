f53f57e9b1584b3fbf1ad41d9f33d3a922b29974
Segni Girma <106369427+segnig@users.noreply.github.com>
2024-12-04 08:21:27 -0800

    Time: 246 ms (9.09%) | Memory: 16.6 MB (7.69%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -2,16 +2,10 @@ class Solution(object):
     def canMakeSubsequence(self, str1, str2):  
         if len(str1) < len(str2):
             return False
-        def match(x):   
-            a = ord("a") 
-            x = ord(x)  
-            dif = x - a + 1
-            res = chr(a + dif % 26)  
-            return res
+        def match(x):      
+            return chr(ord("a") + (ord(x) - ord("a")  + 1) % 26) 
 
         p1 = 0 
-        
-        
         for x in range(len(str2)):  
             while p1 < len(str1) and not (str1[p1] == str2[x] or match(str1[p1]) == str2[x]):  
                 p1 += 1
