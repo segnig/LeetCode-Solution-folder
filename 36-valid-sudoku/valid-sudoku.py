d5a4e2471c9d9b556f83bdf3261e732df0501fb8
Segni Girma <segnigirma11@gmail.com>
2025-02-06 16:07:56 +0300

    Time: 7 ms (39.71%) | Memory: 17.7 MB (81.02%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,24 +1,27 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+
         for row in board:
-            if not self.is_valid_set(row):
+            if not self.is_valid_board(row):
                 return False
+
         for col in range(9):
             column = [board[row][col] for row in range(9)]
-            if not self.is_valid_set(column):
+            if not self.is_valid_board(column):
                 return False
 
         for i in range(0, 9, 3):
             for j in range(0, 9, 3):
                 subgrid = [board[x][y] for x in range(i, i+3) for y in range(j, j+3)]
-                if not self.is_valid_set(subgrid):
+                if not self.is_valid_board(subgrid):
                     return False
         
         return True
-    
-    def is_valid_set(self, cells: List[str]) -> bool:
+
+
+    def is_valid_board(self, sub_board):
         seen = set()
-        for cell in cells:
+        for cell in sub_board:
             if cell != '.':
                 if cell in seen:
                     return False
