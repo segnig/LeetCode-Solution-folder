4e8089db2072007fd8fc1fe6fc05ecd7c0929fb9
Segni Girma <segnigirma11@gmail.com>
2025-02-10 14:41:59 +0300

    Time: 217 ms (11.30%) | Memory: 19.3 MB (7.95%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,11 +1,23 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        right = len(heights)
+        min_h = min(heights)
+        max_h = max(heights)
 
-        for j in range(right):
-            for i in range(j, 0, -1):
-                if heights[i] > heights[i - 1]:
-                    heights[i], heights[i - 1] = heights[i - 1], heights[i]
-                    names[i], names[i - 1] = names[i - 1], names[i]
+        paired = defaultdict(list)
 
-        return names           
+        heights = [h - min_h  for h in heights]
+        order_feq = [0 for _ in range(min_h, max_h + 1)]
+
+        for i in range(len(heights)):
+            paired[heights[i]].append(names[i])
+
+        for index in heights:
+            order_feq[index] += 1
+
+        result = []
+        for index in range(len(order_feq) - 1, -1, -1):
+            if order_feq[index] != 0:
+                for name in paired[index]:
+                    result.append(name)
+
+        return result
