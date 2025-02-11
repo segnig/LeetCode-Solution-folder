5b3f6d8856bbe1ad3213615de6003d8b20b98662
Segni Girma <segnigirma11@gmail.com>
2025-02-11 16:12:51 +0300

    Time: 0 ms (100.00%) | Memory: 18.3 MB (18.40%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -7,7 +7,5 @@ class Solution:
             if cite >= i + 1:
                 h = i + 1
 
-            else: return h
-
-        return h   
-        
+            else: return h  
+        return h
