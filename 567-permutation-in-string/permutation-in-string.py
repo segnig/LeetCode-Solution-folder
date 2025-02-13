264f0461c30f87029a44049bdd27eed0e66b51b1
Segni Girma <segnigirma11@gmail.com>
2025-02-13 16:18:08 +0300

    Time: 16 ms (73.99%) | Memory: 17.9 MB (59.22%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,23 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-        counter1 = defaultdict(int)
-        counter2 = defaultdict(int)
-        for i in range(len(s1)):
-            counter1[s1[i]] += 1
-        for i in range(len(s1)-1):
-            counter2[s2[i]] += 1
+        target = Counter(s1)
+        window =defaultdict(int)
+
+        for char in s2[:len(s1) -1]:
+            window[char] += 1
+
         left = 0
-        right = len(s1) - 1
-        while right < len(s2):
-            counter2[s2[right]] += 1
-            if counter1 == counter2:
+        for right in range(len(s1) - 1, len(s2)):
+            window[s2[right]] += 1
+            if self.anagram(target, window):
                 return True
-            counter2[s2[left]] -= 1
-            if counter2[s2[left]] == 0:
-                del counter2[s2[left]]
+            window[s2[left]] -= 1
             left += 1
-            right += 1
+        return False 
         
-        return False
+
+    def anagram(self, target, window):
+        for char in target:
+            if target[char] != window[char]:
+                return False
+        return True
+        
