5c6ef91827f07f9bbdd4634eee3d66a0fc7875b1
Segni Girma <segnigirma11@gmail.com>
2025-02-14 10:38:27 +0300

    Time: 155 ms (25.96%) | Memory: 18.2 MB (75.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -3,13 +3,18 @@ class Solution:
         target = Counter(t)
         window = Counter()
         res = s
+        poss = len(target)
         left = 0
         for right in range(len(s)):
             window[s[right]] += 1
-            while self.helper(target, window):
+            if window[s[right]] == target[s[right]]:
+                poss -= 1
+            while not poss:
                 if len(res) > right - left + 1:
                     res = s[left:right + 1]
                 window[s[left]] -= 1
+                if window[s[left]] + 1 == target[s[left]]:
+                    poss += 1
                 left += 1
         result_counter = Counter(res)
         return res if self.helper(target, result_counter) else ""
