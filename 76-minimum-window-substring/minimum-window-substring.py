dec7946f239da584a03c8a9ea946a3867093676d
Segni Girma <segnigirma11@gmail.com>
2025-02-14 10:41:10 +0300

    Time: 157 ms (25.15%) | Memory: 18.3 MB (33.90%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,22 +2,19 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         target = Counter(t)
         window = Counter()
-        res = s
-        poss = len(target)
+        res = ""
+        remain_char = len(target)
         left = 0
         for right in range(len(s)):
             window[s[right]] += 1
             if window[s[right]] == target[s[right]]:
-                poss -= 1
-            while not poss:
-                if len(res) > right - left + 1:
+                remain_char -= 1
+            while not remain_char:
+                if len(res) > right - left + 1 or not res:
                     res = s[left:right + 1]
                 window[s[left]] -= 1
                 if window[s[left]] + 1 == target[s[left]]:
-                    poss += 1
+                    remain_char += 1
                 left += 1
         result_counter = Counter(res)
-        return res if self.helper(target, result_counter) else ""
-
-    def helper(self, target, window):
-        return target - window == Counter()
+        return res
