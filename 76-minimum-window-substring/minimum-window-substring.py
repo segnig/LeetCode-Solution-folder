4ab38444bda6db964658e5a88b0059efec4cd6ca
Segni Girma <segnigirma11@gmail.com>
2025-02-14 09:55:53 +0300

    Time: 527 ms (10.72%) | Memory: 18.5 MB (8.00%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,34 +1,21 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
-            return ""
-        
-        cT, win = {}, {}
-        index = []
-        resL = len(s) + 1
+        target = Counter(t)
+        window = defaultdict(int)
+        res = s
         left = 0
-        
-        for char in t:
-            cT[char] = 1 + cT.get(char, 0)
-
-        have, need = 0, len(cT)
-
         for right in range(len(s)):
-            if s[right] in cT:
-                win[s[right]] = 1 + win.get(s[right], 0)
-
-                if win[s[right]] == cT[s[right]]:
-                    have += 1
-
-            while need == have:
-                if right - left + 1 < resL:
-                    resL = right - left + 1
-                    index = [left, right + 1]
-
-                if s[left] in cT:
-                    win[s[left]] -= 1
-                    if win[s[left]] < cT[s[left]]:
-                        have -= 1
+            window[s[right]] += 1
+            while self.helper(target, window):
+                if len(res) > right - left + 1:
+                    res = s[left:right + 1]
+                window[s[left]] -= 1
                 left += 1
+        result_counter = Counter(res)
+        return res if self.helper(target, result_counter) else ""
 
-        return s[index[0]:index[1]] if resL < len(s) + 1 else ""
+    def helper(self, target, window):
+        for char in target:
+            if window[char] < target[char]:
+                return False
+        return True
