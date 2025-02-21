888574bfb356c23b3013ffb364d250741b83e203
Segni Girma <segnigirma11@gmail.com>
2025-02-21 16:34:38 +0300

    Time: 206 ms (9.75%) | Memory: 78.9 MB (8.64%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,32 +1,61 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None 
+        self.prev = None
+        
 class LRUCache:
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.recency = []
-        self.elements = {}
+        self.head = Node(-1)
+        self.tail = Node(-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         
+        self.elements = {}
+        self.nodes = {}
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
         if key not in self.elements:
             return -1
-        self.recency.remove(key)
-        self.recency.append(key)
         
+        node = self.nodes[key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
+        
+        new_node = Node(node.val)
+        self.tail.prev.next = new_node
+        new_node.next = self.tail
+        new_node.prev = self.tail.prev
+        self.tail.prev = new_node
+        
+        self.nodes[key] = new_node
+          
         return self.elements[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self.elements:
             self.elements[key] = value
-            self.recency.remove(key)
+            node = self.nodes[key]
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self.nodes.pop(node.val)
             
         else:
             if self.capacity == len(self.elements):
-                to_be_deleted = self.recency[0]
-                self.recency.pop(0)
-                self.elements.pop(to_be_deleted)
+                to_be_deleted = self.head.next
+                to_be_deleted.prev.next = to_be_deleted.next
+                to_be_deleted.next.prev = to_be_deleted.prev
+                
+                self.elements.pop(to_be_deleted.val)
+            
             self.elements[key] = value
-        self.recency.append(key)
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+             
+        new_node = Node(key)
+        self.tail.prev.next = new_node
+        new_node.next = self.tail
+        new_node.prev = self.tail.prev
+        self.tail.prev = new_node
+        
+        self.nodes[key] = new_node
