3a5b5ab2ea2d56e1551abfc7476cd6cf758301f5
Segni Girma <segnigirma11@gmail.com>
2025-02-27 11:19:08 +0300

    Time: 273 ms (12.07%) | Memory: 27.6 MB (19.01%) - LeetSync

---
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def waysToMakeFair(self, nums: List[int]) -> int:
+        even_indexies = [0]
+        odd_indexies = [0]
+
+        for index, num in enumerate(nums):
+            if index % 2 == 0:
+                even_indexies.append(even_indexies[-1] + num)
+                odd_indexies.append(odd_indexies[-1])
+            else:
+                even_indexies.append(even_indexies[-1])
+                odd_indexies.append(odd_indexies[-1] + num)
+        print(even_indexies)
+        print(odd_indexies)
+
+        result = 0
+
+        for i in range(1, len(nums) + 1):
+            odd = odd_indexies[i - 1] + even_indexies[-1] - even_indexies[i]
+            even = even_indexies[i - 1] + odd_indexies[-1] - odd_indexies[i]
+            if even == odd:
+                result += 1
+
+        return result
