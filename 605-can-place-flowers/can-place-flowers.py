85a67fc7e4ed3475d62be591c0cbdbf2ac1713fd
Segni Girma <segnigirma11@gmail.com>
2025-03-05 14:09:56 +0300

    Time: 7 ms (73.48%) | Memory: 18.1 MB (82.22%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -7,14 +7,10 @@ class Solution:
             if num == 1:
                 break
             left += 1
-
         n -= left // 2 if left < len(flowerbed) else (left + 1)// 2
-        print(left // 2 if left < len(flowerbed) else (left + 1)// 2)
-
         for k in range(left, len(flowerbed)):
             if flowerbed[k] == 1:
                 n -= (zeros_count - 1)// 2 if (zeros_count - 1)// 2 > 0 else 0
-                print((zeros_count - 1)// 2)
                 zeros_count = 0
             else:
                 zeros_count += 1
