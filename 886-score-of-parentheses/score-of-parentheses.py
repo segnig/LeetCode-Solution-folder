beb87d9b7bf7935791231b14ee35f485dde07eaf
Segni Girma <segnigirma11@gmail.com>
2025-03-06 21:28:34 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (7.47%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        result = [0]
+
+        for par in s:
+            if par == "(":
+                result.append(0)
+            else:
+                top = result.pop()
+                if top == 0:
+                    result[-1] += 1
+                else:
+                    result[-1] += 2 * top
+            print(result)
+        return result[-1]
