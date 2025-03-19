f0affa679e326603d652b705a388675c67ea313d
Segni Girma <segnigirma11@gmail.com>
2025-03-19 18:37:53 +0300

    Time: 202 ms (18.30%) | Memory: 72.7 MB (5.09%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,16 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        self.result = set()
-        for i in range(1, n + 1):
-            self.generator(n, k, i, [])
-
+        self.result = []
+        self.generator(n, k, 1, [])
         ans = [list(comb) for comb in self.result]
 
         return ans
         
     def generator(self, n, k, next, comb):
         if len(comb) == k:
-            self.result.add(tuple(comb[::]))
+            self.result.append(comb[::])
             return
         if next > n:
             return
