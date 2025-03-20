eaebaedd4937ba1d34c818df81f438689e7f85a2
Segni Girma <segnigirma11@gmail.com>
2025-03-20 18:12:57 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.15%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -4,12 +4,11 @@ class Solution:
 
         def backtrack(indx=0, comb=[]):
             if indx == len(nums):
-                result.append(comb[:])
+                result.append(comb)
                 return 
-            comb.append(nums[indx])
+            
             backtrack(indx+1, comb)
-            comb.pop()
-            backtrack(indx+1, comb)
+            backtrack(indx+1, comb + [nums[indx]])
         
         backtrack()
 
