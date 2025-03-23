426e08053b34aab26c7fdb4e2b09b5ca0f435504
Segni Girma <segnigirma11@gmail.com>
2025-03-23 13:13:36 +0300

    Time: 751 ms (5.06%) | Memory: 18 MB (18.97%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,10 +1,32 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        result, stack = [], []
-        for index in range(len(pattern) + 1):
-            stack.append(index + 1)
+        n = len(pattern) + 1
+        self.result = []
+        self.backtrack(pattern, [], set())
+        return ''.join(self.result)
+    
+    def backtrack(self, pattern, nums, used_digits):
+        if len(nums) == len(pattern) + 1:
+            res = ''.join(nums)
 
-            while stack and (index == len(pattern) or pattern[index] == "I"):
-                result.append(str(stack.pop()))
+            if not self.result or res < ''.join(self.result):
+                self.result = nums[:]
+            return
 
-        return "".join(result)
+        for i in range(1, len(pattern) + 2):
+            if str(i) not in used_digits:
+                nums.append(str(i))
+                used_digits.add(str(i))
+                if self.isValid(pattern, nums):
+                    self.backtrack(pattern, nums, used_digits)
+                nums.pop()
+                used_digits.remove(str(i))
+    
+
+    def isValid(self, pattern, nums):
+        for i in range(len(nums) - 1):
+            if pattern[i] == 'I' and nums[i] >= nums[i + 1]:
+                return False
+            if pattern[i] == 'D' and nums[i] <= nums[i + 1]:
+                return False
+        return True
