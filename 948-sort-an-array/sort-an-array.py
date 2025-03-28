46779674a7ba376985482843ee7f9bbd3b0e33ce
Segni Girma <segnigirma11@gmail.com>
2025-03-28 19:17:24 +0300

    Time: 580 ms (66.45%) | Memory: 25.8 MB (31.43%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,15 +1,31 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        max_num = max(nums)
-        min_num = min(nums)
-        count_array = [0 for _ in range(min_num, max_num + 1)]
+        return self.divide(0, len(nums) - 1, nums)
+        
 
-        for num in nums:
-            count_array[num - min_num] += 1
-        idx = 0
-        for index in range(len(count_array)):
-            while count_array[index] > 0:
-                nums[idx] = index + min_num
-                idx += 1
-                count_array[index] -= 1
-        return nums   
+    def merge(self, left_arr, right_arr):
+        sorted_arr = []
+        left , right = 0, 0
+
+        while left < len(left_arr) and right < len(right_arr):
+            if left_arr[left] <= right_arr[right]:
+                sorted_arr.append(left_arr[left])
+                left += 1
+            else:
+                sorted_arr.append(right_arr[right])
+                right += 1
+        if left < len(left_arr):
+            sorted_arr.extend(left_arr[left:])
+        else:
+            sorted_arr.extend(right_arr[right:])
+
+        return sorted_arr
+    def divide(self, left, right, nums):
+        if left == right:
+            return [nums[left]]
+
+        mid = (right + left) // 2
+        left_nums = self.divide(left, mid, nums)
+        right_nums = self.divide(mid + 1, right, nums)
+
+        return self.merge(left_nums, right_nums)
