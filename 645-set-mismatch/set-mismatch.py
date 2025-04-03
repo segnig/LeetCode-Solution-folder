a1a5fcf044849950b55a3f8fc016059cd96cb4a9
Segni Girma <segnigirma11@gmail.com>
2025-04-03 19:50:51 +0300

    Time: 19 ms (42.18%) | Memory: 19.2 MB (50.63%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        result = []
+
+        for i in range(len(nums)):
+            while nums[i] != i + 1:
+                correct_position = nums[i] - 1
+                if nums[i] == nums[correct_position]:
+                    break
+                nums[i], nums[correct_position] = nums[correct_position], nums[i]
+            
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                result.extend([nums[i], i+1])
+        return result
