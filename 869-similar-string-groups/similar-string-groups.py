ab3238d990ab3e4635448a675c1d45b699438133
Segni Girma <segnigirma11@gmail.com>
2025-05-10 20:07:04 +0300

    Time: 131 ms (81.99%) | Memory: 18.2 MB (49.79%) - LeetSync

--- 869-similar-string-groups/similar-string-groups.py
+++ 869-similar-string-groups/similar-string-groups.py
@@ -1,32 +1,50 @@
-class Solution:
-    def numSimilarGroups(self, strs: List[str]) -> int:
-        self.visited = set()
-        result = 0
-
-        for s in strs:
-            if s not in self.visited:
-                self.dfs(s, strs)
-                result += 1
-        
-        return result
+class UnionFind:
+    def __init__(self, strs):
+        self.parent = {word : word for word in strs}
+        self.size = {word : 1 for word in strs}
     
-    def dfs(self, s, strs):
-        if s in self.visited:
-            return 
-        
-        self.visited.add(s)
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
 
-        for nbr in strs:
-            if self.check(s, nbr):
-                self.dfs(nbr, strs)
+        return self.parent[x]
 
+    def union(self, u, v):
+        p_v = self.find(v)
+        p_u = self.find(u)
 
-    def check(self, s, nbr):
+        if p_v == p_u:
+            return False
+
+        if self.size[p_v] > self.size[p_u]:
+            self.parent[p_u] = p_v
+            self.size[p_v] += self.size[p_u]
+        else:
+            self.parent[p_v] = p_u
+            self.size[p_u] += self.size[p_v]
+
+        return True
+
+class Solution:
+
+    def check(self, s1, s2):
         count = 0
-        for i in range(len(s)):
-            if s[i] != nbr[i]:
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
                 count += 1
-            
             if count > 2:
                 return False
-        return count == 2 or count == 0
+
+        return count == 0 or count == 2
+
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        uf = UnionFind(set(strs))
+        result = len(set(strs))
+        for i in range(len(strs)):
+            for j in range(i + 1, len(strs)):
+                if self.check(strs[i], strs[j]):
+                    if uf.union(strs[i], strs[j]):
+                        result -= 1
+
+        return result
