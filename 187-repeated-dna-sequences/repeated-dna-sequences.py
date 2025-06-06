92232fcefc0221be77ab38ca66505ef35a90fe26
Segni Girma <segnigirma11@gmail.com>
2025-06-06 09:19:31 +0300

    Time: 23 ms (71.61%) | Memory: 29.4 MB (15.35%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        result = set()
+        canditates = set()
+
+        for i in range(10, len(s) + 1):
+            cand = s[i-10:i]
+            if cand in canditates:
+                result.add(cand)
+            canditates.add(cand)
+
+        result = list(result)
+
+        return result
