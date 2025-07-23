0765c1f0a2aebd533ce963454aa17ec86037a427
Segni Girma <segnigirma11@gmail.com>
2025-07-23 00:40:08 -0700

    Time: 395 ms (5.02%) | Memory: 42.9 MB (5.31%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,11 +1,39 @@
+class UnionFind:
+    def __init__(self, nums):
+        self.parent = {num:num for num in nums}
+        self.size = {num:1 for num in nums}
+
+    def find(self, x):
+        if x != self.parent[x]:
+            return self.find(self.parent[x])
+        return x
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+
+        if self.parent[root_v] == self.parent[root_u]:
+            return 
+        
+        if self.size[root_v] > self.size[root_u]:
+            self.parent[root_u] = self.parent[root_v]
+            self.size[root_v] += self.size[root_u]
+        else:
+            self.parent[root_v] = self.parent[root_u]
+            self.size[root_u] += self.size[root_v]
+
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = set(nums)
-        longest = 0
-        for n in nums:
-            if (n - 1) not in nums:
-                length = 1
-                while (n + length) in nums:
-                    length += 1
-                longest = max(length, longest)
-        return longest
+        uf = UnionFind(nums)
+
+        for num in nums:
+            if num + 1 in uf.parent:
+                uf.union(num + 1, num)
+            if num - 1 in uf.parent:
+                uf.union(num - 1, num)
+        result = 0
+        if nums:
+            result = max([m for _,  m in uf.size.items()])
+
+        return result
