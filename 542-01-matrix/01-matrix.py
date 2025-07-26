b61e049a55446ac82289cf1a590ed7261ca2a3f4
Segni Girma <segnigirma11@gmail.com>
2025-07-25 22:11:44 -0700

    Time: 185 ms (31.90%) | Memory: 20.4 MB (64.62%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,34 +1,26 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        
-        result = [[0] * len(mat[0])  for _ in range(len(mat))]
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        result = [[0] * len(mat[0]) for _ in range(len(mat))]
 
         queue = deque()
-        
-        far = 1
-
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if mat[i][j] == 0:
                     queue.append((i, j))
-
+        
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        far = 1
         while queue:
-
             for _ in range(len(queue)):
-                row, col = queue.popleft()
-
-                for dr, dc in directions:
-                    n_row, n_col = row + dr, col + dc
-
-                    if self.inbound(len(mat), len(mat[0]), n_row, n_col) and mat[n_row][n_col] == 1 and result[n_row][n_col] == 0:
-                        result[n_row][n_col] = far
-                        queue.append((n_row, n_col))   
+                x, y = queue.popleft()
+                for dx, dy in directions:
+                    xx, yy = x + dx, y + dy
+                    if self.inbound(mat, xx, yy) and result[xx][yy] == 0 and mat[xx][yy]==1:
+                        queue.append((xx, yy))
+                        result[xx][yy] = far
+                
             far += 1
+        return result
 
-        return result           
-
-    def inbound(self, row, col, r, c):
-        return 0 <= r < row and 0 <= c < col 
-
-
+    def inbound(self, mat, x, y):
+        return 0 <= x < len(mat) and 0 <= y < len(mat[0])
