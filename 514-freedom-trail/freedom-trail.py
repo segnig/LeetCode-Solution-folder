eebe900e1f5e4ed27c31e93b08bd2ada1aec5768
Segni Girma <segnigirma11@gmail.com>
2025-07-27 11:29:45 +0300

    Time: 194 ms (30.98%) | Memory: 18.5 MB (35.28%) - LeetSync

--- 514-freedom-trail/freedom-trail.py
+++ 514-freedom-trail/freedom-trail.py
@@ -1,24 +1,20 @@
-from collections import defaultdict
-
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
+
         memo = {}
-        positions = defaultdict(list)
-        for i, c in enumerate(ring):
-            positions[c].append(i)
-        return self.helper(0, 0, positions, key, ring, memo)
-    
-    def helper(self, in_index, pos, positions, key, ring, memo):
-        if in_index == len(key):
-            return 0
-        if (in_index, pos) in memo:
-            return memo[(in_index, pos)]
-        min_steps = float('inf')
-        for i in positions[key[in_index]]:
-            if i >= pos:
-                steps = min(i - pos, pos + len(ring) - i)
-            else:
-                steps = min(pos - i, i + len(ring) - pos)
-            min_steps = min(min_steps, steps + self.helper(in_index + 1, i, positions, key, ring, memo))
-        memo[(in_index, pos)] = min_steps + 1
-        return min_steps + 1
+        
+        def helper(r, k):
+            if k == len(key):
+                return 0
+            if (r, k) in memo:
+                return memo[(r, k)]
+            
+            result = inf
+            for i, char in enumerate(ring):
+                if key[k] == char:
+                    min_dist = min(abs(i - r), len(ring) - abs(i - r)) + 1
+                    result = min(result, min_dist + helper(i, k + 1))
+            memo[(r, k)] = result
+            return result
+        return helper(0, 0)
+        
