2c4ee713922c9b5ee889278812282d36f07f6db9
Segni Girma <segnigirma11@gmail.com>
2025-07-28 08:23:34 +0300

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.49%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if (n < 0){
+            x = 1 / x;
+            N = -N;
+        }
+        return pow(x, n);
+        
+    }
+private:
+    double pow(double x, int n){
+        if (n == 0)return 1.0;
+        double half = pow(x, n / 2);
+        if (n % 2 == 0) return half *= half;
+        else {
+            return half * half * x;
+        }
+        
+    }
+};
