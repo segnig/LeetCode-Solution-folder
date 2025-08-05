4ac5cb3aa570691d20959727fb2c2bc459cd1391
Segni Girma <segnigirma11@gmail.com>
2025-08-05 16:10:20 +0300

    Time: 74 ms (14.95%) | Memory: 38 MB (89.89%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        prefix_sum = []
+        pre_sum = []
         hash_map = {0:-1}
 
         for i, num in enumerate(nums):
-            if not prefix_sum:
-                prefix_sum.append(num % k)
+            if not pre_sum:
+                pre_sum.append(num % k)
             else:
-                prefix_sum.append((prefix_sum[-1] + num )% k)
-            if prefix_sum[-1] not in hash_map:
-                hash_map[prefix_sum[-1]] = i
-            elif i - hash_map[prefix_sum[-1]] > 1:
+                pre_sum.append((pre_sum[-1] + num )% k)
+            if pre_sum[-1] not in hash_map:
+                hash_map[pre_sum[-1]] = i
+            elif i - hash_map[pre_sum[-1]] > 1:
                 return True
 
         return False
