740a55844a610aa86ede0ad143c579f1953d454c
Segni Girma <segnigirma11@gmail.com>
2025-08-06 09:36:40 +0300

    Time: 72 ms (47.17%) | Memory: 62.1 MB (6.41%) - LeetSync

--- 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
@@ -1,19 +1,18 @@
 class DataStream:
 
     def __init__(self, value: int, k: int):
-        self.val = value
+        self.store = []
         self.k = k
-        self.n = 0
+        self.value = value
         
 
-    def consec(self, num: int) -> bool:
-        if num == self.val:
-            self.n += 1
+    def consec(self, num: int) -> bool: 
+        if not self.store or self.store[-1][0] != num:
+            self.store.append([num, 1])
         else:
-            self.n = 0
-        return self.n >= self.k
+            self.store[-1][1] += 1
         
-
+        return False if not self.store else self.store[-1][1] >= self.k and self.store[-1][0] == self.value
 
 # Your DataStream object will be instantiated and called as such:
 # obj = DataStream(value, k)
