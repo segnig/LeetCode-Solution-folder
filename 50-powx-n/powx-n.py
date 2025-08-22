9b20504d30b5b5221a9a3608cd91057eae68229f
Segni Girma <segnigirma11@gmail.com>
2025-08-22 18:31:50 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (31.25%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -5,9 +5,7 @@ class Solution:
         def power(x, n):
             if n == 1:
                 return x
-            
             res = self.myPow(x, n//2)
             res *= res
             return res * x if n % 2 == 1 else res
-
         return power(x, n) if n > 0 else 1/power(x, -n)
