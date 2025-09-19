d0d3a5cb964e53b857ddf7b3c6cdbc91983845f4
Segni Girma <segnigirma11@gmail.com>
2025-09-19 10:40:21 +0300

    Time: 56 ms (93.86%) | Memory: 49.6 MB (53.03%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,22 +1,25 @@
-class Solution(object):
-    def getAncestors(self, n, edges):
-        res = [[] for _ in range(n)]
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        in_degree = [0] * n
         graph = [[] for _ in range(n)]
+        ancestors = [set() for _ in range(n)]
         
-        for edge in edges:
-            graph[edge[0]].append(edge[1])
+        for u, v in edges:
+            graph[u].append(v)
+            in_degree[v] += 1
         
+        q = deque()
         for i in range(n):
-            self.dfs(graph, i, i, res, [False] * n)
+            if in_degree[i] == 0:
+                q.append(i)
+                
+        while q:
+            u = q.popleft()
+            for v in graph[u]:
+                ancestors[v].add(u)
+                ancestors[v].update(ancestors[u])
+                in_degree[v] -= 1
+                if in_degree[v] == 0:
+                    q.append(v)
         
-        for i in range(n):
-            res[i].sort()
-        
-        return res
-    
-    def dfs(self, graph, parent, curr, res, visit):
-        visit[curr] = True
-        for dest in graph[curr]:
-            if not visit[dest]:
-                res[dest].append(parent)
-                self.dfs(graph, parent, dest, res, visit)
+        return [sorted(ancestors[i]) for i in range(n)]
