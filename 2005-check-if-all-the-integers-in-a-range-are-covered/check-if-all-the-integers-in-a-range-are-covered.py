af03dc6d8405892c4648e282fd5e13647c02d0c4
Segni Girma <segnigirma11@gmail.com>
2025-09-21 04:24:16 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.97%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,13 +1,12 @@
-class Solution(object):
-    def isCovered(self, ranges, left, right):
-        set1 = set()
-
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        store = set()
         for l, r in ranges:
             for num in range(l, r + 1):
-                set1.add(num)
-
+                store.add(num)
+        
         for num in range(left, right + 1):
-            if num not in set1:
+            if num not in store:
                 return False
-
+        
         return True
