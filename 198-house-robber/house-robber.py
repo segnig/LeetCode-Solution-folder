3442438d7e64f28aa6bcc48bf57423d4eba7110d
Segni Girma <segnigirma11@gmail.com>
2025-09-24 18:06:30 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.99%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            
+            if i == 1:
+                return max(nums[0], nums[1])
+            
+            rob = nums[i] + dp(i - 2)
+            dont_rob = dp(i - 1)
+
+            return max(rob, dont_rob)
+        
+        return dp(len(nums) - 1)
