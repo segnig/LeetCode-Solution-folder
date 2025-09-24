97db8d19fc97a3e660abddb2e622d8b5b95e7660
Segni Girma <segnigirma11@gmail.com>
2025-09-24 16:50:10 +0300

    Time: 30 ms (94.58%) | Memory: 17.8 MB (40.87%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,11 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}
     def fib(self, n: int) -> int:
-        memo = {}
-        def fn(n):
-            if n < 2:
-                return n
-            if n not in memo:
-                memo[n] =  fn(n-1) + fn(n-2)
-            return memo[n]
+        if n < 2:
+            return n
+        if n not in self.memo:
+            self.memo[n] =  self.fib(n-1) + self.fib(n-2)
+        return self.memo[n]
         
-        return fn(n)
+       
