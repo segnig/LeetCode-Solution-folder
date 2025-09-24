66ccf742695db53d497b2c455fd2e40fd7770784
Segni Girma <segnigirma11@gmail.com>
2025-09-24 17:19:25 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.61%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        
+        def rec(n):
+            if n <= 2:
+                return n
+            if n not in memo:
+                memo[n] = rec(n-1) + rec(n-2)
+        
+            return memo[n]
+
+        return rec(n)
+        
