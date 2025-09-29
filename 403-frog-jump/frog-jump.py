535f640e34486fb2d1937b4ba332eddc4de834cd
Segni Girma <segnigirma11@gmail.com>
2025-09-29 15:53:35 +0300

    Time: 47 ms (87.11%) | Memory: 26.9 MB (24.61%) - LeetSync

---
+++ 403-frog-jump/frog-jump.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        if stones[1] != 1:
+            return False
+        
+        temp = set(stones)
+        hashTable = {stone: i for i, stone in enumerate(stones)}
+
+        @cache
+        def dp(k, i):
+            if i == len(stones) - 1:
+                return True
+            
+            stone = stones[i]
+            for step in [k-1, k, k+1]:
+
+                if (step != 0) and (stone + step in temp) and dp(step, hashTable[stone + step]):
+                    return True
+
+            return False
+
+        return dp(1, 1)
