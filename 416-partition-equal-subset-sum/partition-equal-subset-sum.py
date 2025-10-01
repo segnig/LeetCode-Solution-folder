3f0e2233b77b958b312ff09295eef84c809e262d
Segni Girma <segnigirma11@gmail.com>
2025-10-01 14:56:27 +0300

    Time: 1360 ms (33.81%) | Memory: 497.9 MB (5.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,15 +1,33 @@
-class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 == 1:
-            return False
-        target = sum(nums) / 2
+# class Solution:
+#     def canPartition(self, nums: List[int]) -> bool:
+#         if sum(nums) % 2 == 1:
+#             return False
+#         target = sum(nums) / 2
 
-        @cache
-        def dp(idx, sub1):
-            if idx == len(nums):
-                return target == sub1
+#         @cache
+#         def dp(idx, sub1):
+#             if idx == len(nums):
+#                 return target == sub1
             
 
-            return dp(idx + 1, sub1 + nums[idx]) or dp(idx + 1, sub1)
+#             return dp(idx + 1, sub1 + nums[idx]) or dp(idx + 1, sub1)
 
-        return dp(0, 0)
+#         return dp(0, 0)
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total // 2
+        
+        @cache
+        def dfs(i, rem):
+            if rem == 0:
+                return True
+            if i >= len(nums) or rem < 0:
+                return False
+            
+            return dfs(i + 1, rem) or dfs(i + 1, rem - nums[i])
+
+        return dfs(0, target)
