561b7cfeabfe3f7805e8e21d3b9ef63f893de9c3
Segni Girma <segnigirma11@gmail.com>
2025-10-03 10:18:01 +0300

    Time: 231 ms (5.31%) | Memory: 18.4 MB (6.10%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.py
@@ -0,0 +1,19 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+
+        cups = [[0]* (query_row + 2) for _ in range(query_row + 2)]
+
+        cups[1][1] = poured
+
+        for row in range(2, query_row+2):
+            for col in range(1, query_row+2):
+                left = cups[row-1][col-1] - 1 if cups[row-1][col-1] > 1 else 0
+                right = cups[row-1][col] - 1 if cups[row-1][col] > 1 else 0
+                cups[row][col] = left / 2 + right / 2
+
+
+
+        for cup in  cups:
+            print(cup)
+        
+        return cups[query_row+1][query_glass+1] if cups[query_row+1][query_glass+1] < 1 else 1
