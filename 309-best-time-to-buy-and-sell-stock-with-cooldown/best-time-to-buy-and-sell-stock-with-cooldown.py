48fc8cc327818e5db33cdebcdb355528cefd446e
Segni Girma <segnigirma11@gmail.com>
2025-10-07 09:41:40 +0300

    Time: 7 ms (41.86%) | Memory: 19.2 MB (17.07%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,9 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @cache
+        memo ={}
         def dp(i, is_buy):
             if i >= len(prices):
                 return 0
+
+            if (i, is_buy) in memo:
+                return memo[(i, is_buy)]
 
             buy, sell, not_buy, not_sell = 0, 0, 0, 0
             if is_buy:
@@ -12,6 +15,7 @@ class Solution:
             else:
                 sell = buy = prices[i] + dp(i+2, not is_buy)
                 not_sell = dp(i+1, is_buy)
-            return max([buy, not_buy, sell, not_sell])
+            memo[(i, is_buy)] = max([buy, not_buy, sell, not_sell])
+            return memo[(i, is_buy)]
         
         return dp(0, True)
