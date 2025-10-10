cffee5d49f0cb8b065e6d4fe082cbf9562f98ba6
Segni Girma <segnigirma11@gmail.com>
2025-10-10 09:17:23 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (8.98%) - LeetSync

---
+++ 1137-height-checker/height-checker.py
@@ -0,0 +1,10 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        count = 0
+        sorted_heights = sorted(heights)
+
+        for i in range(len(heights)):
+            if sorted_heights[i] != heights[i]:
+                count += 1
+        
+        return count
