e0a5f29209bc7bea82f4acbdbe7c34138742d17c
Segni Girma <segnigirma11@gmail.com>
2025-10-15 16:36:37 +0300

    Time: 79 ms (62.67%) | Memory: 18.1 MB (33.05%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,15 +1,28 @@
-class Solution(object):
-    def minSteps(self, s, t):
-        counter_s = defaultdict(int)
-        counter_t = defaultdict(int)
-
-        for i in range(len(t)):
-            counter_t[t[i]] += 1
-            counter_s[s[i]] += 1
-            
-        res = 0
-        for k in counter_s:
-            if counter_s[k] > counter_t[k]:
-                res += counter_s[k] - counter_t[k]
-
-        return res
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        """
+        s = "bab", t = "aba"
+        bababa
+        a:3 b:3
+        
+        s = {b:2, a:1, c:2}  t = {a:3, b:1, c:1} a=2 b=1 c=1 
+        need = 2 
+        have = 2
+        
+        s = "leetcode", t = "practice" 
+        {l:1, e:3, t:1, o:1, d:1, c:1}    {p:1, c:2, a:1, t:1, i:1, e:1, r:1}
+        need=  1 + 2 + 1 + 1
+        have=  1 + 1 + 1 + 1 + 1 
+        
+        """ 
+        freq_s = Counter(s)
+        freq_t = Counter(t)
+        
+        
+        # bababa = ab
+        
+        need = 0
+        for char in set(s+t):
+            need += abs(freq_s[char] - freq_t[char])
+        
+        return need // 2
