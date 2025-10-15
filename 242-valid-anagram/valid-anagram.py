8f9ab5868df30191ec34926e9f98151cc9c4799f
Segni Girma <segnigirma11@gmail.com>
2025-10-15 15:15:11 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (86.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def isAnagram(self, s, t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
+        
         s_counter = Counter(s)
         t_counter = Counter(t)
-
         for char in s_counter:
             if s_counter[char] != t_counter[char]:
                 return False
-
-        return True      
+        return True
