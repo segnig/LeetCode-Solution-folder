bcd115b90af83bd7ce008d1e90971b482dec7d27
Segni Girma <segnigirma11@gmail.com>
2025-10-17 12:45:06 +0300

    Time: 63 ms (57.92%) | Memory: 32.9 MB (22.28%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        prefix_sum = [0]
-        for num in nums:
-            prefix_sum.append(prefix_sum[-1] + num)
-        
-        left, right = 0, 1
         result = max(nums)
-        
-        for right in range(1, len(prefix_sum)):
-            if prefix_sum[right] - prefix_sum[left] < 0:
-                left = right
-            else:
-                result = max(result, prefix_sum[right] - prefix_sum[left])
+        prefix = 0
+        for num in nums:
+            prefix += num
+            result = max(result, prefix)
+            
+            if prefix < 0:
+                prefix = 0
+
         return result
