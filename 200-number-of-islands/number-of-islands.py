df81bc56b0f5dce5056a3b8248e54c5e856a19fe
Segni Girma <segnigirma11@gmail.com>
2025-10-17 16:24:30 +0300

    Time: 267 ms (32.30%) | Memory: 25.4 MB (26.41%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        self.ROWS = len(grid)
+        self.COLS = len(grid[0])
+        self.VISITED = set()
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         self.grid = grid
         count = 0
 
-        for r in range(len(self.grid)):
-            for c in range(len(self.grid[0])):
-                if self.grid[r][c] == "1":
-                    self.dfs(r, c)
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if grid[row][col] == "1" and (row, col) not in self.VISITED:
+                    self.dfs(row, col)
                     count += 1
-        return count
-
         
-    def in_bound(self, row, col):
-        return 0 <= row < len(self.grid) and 0 <= col < len(self.grid[0])
+        return count
+        
 
     def dfs(self, row, col):
-        if not self.in_bound(row, col) or self.grid[row][col] == "0":
+        if col < 0 or col >= self.COLS or row < 0 or row >= self.ROWS:
+            return 
+        if self.grid[row][col] == "0":
             return
-        if self.grid[row][col] == -1:
+        if (row, col) in self.VISITED:
             return
 
-        self.grid[row][col] = -1
-        self.dfs(row - 1, col) 
-        self.dfs(row + 1, col) 
-        self.dfs(row, col - 1)  
-        self.dfs(row, col + 1)    
+        self.VISITED.add((row, col))
+        for dr, dc in self.directions:
+            self.dfs(row+dr, col+dc)
