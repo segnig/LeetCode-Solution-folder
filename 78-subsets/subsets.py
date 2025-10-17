65aac3efa93fa6864b175c625f3282ed7fdf5ed2
Segni Girma <segnigirma11@gmail.com>
2025-10-17 08:21:59 +0300

    Time: 11 ms (1.67%) | Memory: 18.1 MB (21.35%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,15 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
 
-        n = len(nums)
+        subsets = set()
+        
+        def backtrack(i, subset):
+            nonlocal subsets
+            if i == len(nums):
+                subsets.add(tuple(subset[::]))
+                return
+            
+            for k in range(i, len(nums)):
+                subset.append(nums[k])
+                backtrack(k+1, subset)
+                subset.pop()
+                backtrack(k+1, subset)
 
-        for num in range(2 ** n):
-            comb = []
-            for index in range(n):
-                if index == num:
-                    break
-                if num & (1 << index )!= 0:
-                    comb.append(nums[index])
-            result.append(comb.copy())
-        return result
+
+        backtrack(0, [])
+
+        return [list(subset) for subset in subsets]
+        
