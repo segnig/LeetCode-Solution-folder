f7e2893c36fcbec9ee7f9571209f99ee7bc81e4a
Segni Girma <segnigirma11@gmail.com>
2025-10-19 13:07:20 +0300

    Time: 7 ms (57.47%) | Memory: 17.7 MB (99.46%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,18 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        w1 = {}
-        for i in range(len(s)):
-            if s[i] in w1:
-                if t[i] != w1[s[i]]:
-                    return False
-            w1[s[i]] = t[i]
-        w1 = {}
-        for i in range(len(s)):
-            if t[i] in w1:
-                if s[i] != w1[t[i]]:
-                    return False
-            w1[t[i]] = s[i]
-        return True
+        replace_s = {}
+        replace_t = {}
 
+
+        for i in range(len(s)):
+            if s[i] not in replace_s:
+                replace_s[s[i]] = t[i]
+            if t[i] not in replace_t:
+                replace_t[t[i]] = s[i]
+            
+            if t[i] != replace_s[s[i]] or s[i] != replace_t[t[i]]:
+                return False
         
-        
+        return True
