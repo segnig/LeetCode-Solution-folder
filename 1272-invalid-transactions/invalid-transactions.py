f31ee0f49f990b1e6cdc8c992315136311689638
Segni Girma <segnigirma11@gmail.com>
2025-10-19 11:32:02 +0300

    Time: 7 ms (95.85%) | Memory: 18.4 MB (65.72%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,29 +1,37 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        parsed = []
-        for t in transactions:
-            name, time, amount, city = t.split(',')
-            parsed.append([name, int(time), int(amount), city, t])
         
-        result = set()
+        name_to_transactions = defaultdict(list)
+
+        for index, transaction in enumerate(transactions):
+            name, time, amount, city = transaction.split(",")
+            time, amount = int(time), int(amount)
+
+            name_to_transactions[name].append((time, amount, city, index))
+
+        invalid_transactions = set()
+
+        for name, transaction in name_to_transactions.items():
+            transaction.sort()
+
+            for i, tran in enumerate(transaction):
+                time, amount, city, index = tran
+
+                if amount > 1000:
+                    invalid_transactions.add(index)
         
-        for i in range(len(parsed)):
-            name_i, time_i, amount_i, city_i, orig_i = parsed[i]
-            
-            if amount_i > 1000:
-                result.add(orig_i + f"#{i}")
-                continue
+                
+                for j in range(i+1, len(transaction)):
+                    if city != transaction[j][2] and transaction[j][0] - time <= 60:
+                        invalid_transactions.add(index)
+                        invalid_transactions.add(transaction[j][-1])
+                    elif transaction[j][0] - time > 60:
+                        break
+        
+        result = []
 
-            for j in range(len(parsed)):
-                if i == j:
-                    continue
-                    
-                name_j, time_j, amount_j, city_j, orig_j = parsed[j]
-                if name_i == name_j and city_i != city_j and abs(time_i - time_j) <= 60:
-                    result.add(orig_i+f"#{i}")
-                    result.add(orig_j+ f"#{j}")
+        for i in range(len(transactions)):
+            if i in invalid_transactions:
+                result.append(transactions[i])
 
-        return list([res.split("#")[0]  for res in result])
+        return result
