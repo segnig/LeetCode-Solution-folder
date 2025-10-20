df3b656f6918d6e57e30d7e00279c05a51801bf9
Segni Girma <segnigirma11@gmail.com>
2025-10-20 15:38:49 +0300

    Time: 46 ms (89.67%) | Memory: 18.3 MB (15.73%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,10 +1,9 @@
-class Solution(object):
-    def firstUniqChar(self, s):
-        counter_s = Counter(s)
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counter = Counter(s)
 
-        for i, char in enumerate(s):
-            if counter_s[char] == 1:
-                return i
-        return-1
+        for index, char in enumerate(s):
+            if counter[char] == 1:
+                return index
 
-        
+        return -1        
