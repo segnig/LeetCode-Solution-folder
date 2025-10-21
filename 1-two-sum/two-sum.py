5cedb8c4ea7530b981d9f896cad1daff61981cd0
Segni Girma <segnigirma11@gmail.com>
2025-10-21 18:09:33 +0300

    Time: 0 ms (100.00%) | Memory: 19 MB (23.70%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,20 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        nums_index = []
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
 
-        for i, n in enumerate(nums):
-            nums_index.append((n, i))
-
-        nums_index.sort()
-
-        left = 0
-        right = len(nums_index) - 1
-
-        while left < right:
-            if target == nums_index[left][0] + nums_index[right][0]:
-                return [nums_index[left][1], nums_index[right][1]]
-            elif target > nums_index[left][0] + nums_index[right][0]:
-                left += 1
-            else:
-                right -= 1
-        
+        for index, num in enumerate(nums):
+            if target - num in store:
+                return [store[target - num], index]
+            store[num] = index
