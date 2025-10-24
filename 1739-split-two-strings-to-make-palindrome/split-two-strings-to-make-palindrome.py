a5007cd6ae1670327322c7e36ff8baf3404f986b
Segni Girma <segnigirma11@gmail.com>
2025-10-24 18:33:57 +0300

    Time: 22 ms (83.69%) | Memory: 18.1 MB (78.72%) - LeetSync

--- 1739-split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.py
+++ 1739-split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.py
@@ -4,14 +4,12 @@ class Solution:
 
         index = 0
         while index < len(a) - index and a[index] == b[~index]: index += 1
-        if check_palindrome(a[:index] + b[index:]) or check_palindrome(a[:-index] + b[-index:]): return True
+        if check_palindrome(a[:index] + b[index:]) or check_palindrome(a[:-index] + b[-index:]): 
+            return True
 
-        print(a[:-index] + b[-index:])
-        print(a[:index] + b[index:])
         index = 0
         while index < len(a) - index and b[index] == a[~index]: index += 1
-        print(b[:-index] + a[-index:])
-        print(b[:index] + a[index:])
-        if check_palindrome(b[:index] + a[index:]) or check_palindrome(b[:-index] + a[-index:]): return True
+        if check_palindrome(b[:index] + a[index:]) or check_palindrome(b[:-index] + a[-index:]): 
+            return True
 
         return False
