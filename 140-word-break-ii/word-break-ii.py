f6e0e2eae350a8b57ce25f6e001bae3f763194c6
Segni Girma <segnigirma11@gmail.com>
2025-10-27 10:18:27 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (67.89%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        word_set = set(wordDict)
+        memo = {}
+
+        def dp(start):
+            if start in memo:
+                return memo[start]
+            
+            if start == len(s):
+                return [""]
+
+            sentences = []
+
+            for end in range(start+1, len(s)+1):
+                word = s[start:end]
+
+                if word in word_set:
+                    for sub in dp(end):
+                        sentences.append(word + (" " + sub if sub else ""))
+            
+            memo[start] = sentences
+            return sentences
+
+        return dp(0)
