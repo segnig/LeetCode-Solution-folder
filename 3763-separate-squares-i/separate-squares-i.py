57b4eb2c794273d353209f330f2012803ea43f63
Segni Girma <segnigirma11@gmail.com>
2025-10-29 10:36:44 +0300

    Time: 2926 ms (20.15%) | Memory: 45.8 MB (98.53%) - LeetSync

---
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -0,0 +1,33 @@
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        
+        def helper(mid):
+
+            difference = 0
+
+            for x0, y0, length in squares:
+
+                yf = y0 + length
+
+                if yf > mid > y0:
+                    difference += (mid - y0) * length
+                    difference -= (yf - mid) * length
+                elif yf <= mid:
+                    difference += length * length
+                else:
+                    difference -= length * length
+
+            return difference >= 0
+
+        
+        right, left = 10 ** 9 + 1, 0
+
+        while right - left >= 0.000001:
+            mid = left + (right- left) / 2
+
+            if helper(mid):
+                right = mid
+            else:
+                left = mid
+            print(right, left, mid)
+        return left 
