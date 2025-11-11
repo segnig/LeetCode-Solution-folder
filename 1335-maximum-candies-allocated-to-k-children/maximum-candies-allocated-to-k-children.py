4de8459a68487de785737ddaa9a67033a55ba677
Segni Girma <segnigirma11@gmail.com>
2025-11-11 17:42:16 +0300

    Time: 345 ms (62.53%) | Memory: 29.6 MB (85.63%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -1,19 +1,21 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-
         left, right = 1, max(candies)
+
+        def checker(x):
+            count = 0
+            for candy in candies:
+                count += candy // x
+            
+            return count >= k
+        
         while left <= right:
-            mid = left + (right - left) // 2
-            if self.check(candies, k, mid):
+            mid = (right + left) // 2
+            
+            if checker(mid):
                 left = mid + 1
             else:
                 right = mid - 1
-        return right
-
-
-    def check(self, candies, k, per_child):
-        count = 0
-        for cand in candies:
-            count += cand // per_child
         
-        return count >= k
+        return right
+        
