9160f0c9687a450591b9c25615880e36ca986dce
Segni Girma <segnigirma11@gmail.com>
2025-11-14 18:04:30 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (27.00%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,7 +1,5 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # [[10,20],[30,200],[400,50],[30,20]]
-        #   10, 170, -350, -10
         
         costs.sort(key =lambda cost: cost[1] - cost[0])
 
