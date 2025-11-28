70459d0ee87b52593ebc74fe1f3c3b6503bc07f9
Segni Girma <segnigirma11@gmail.com>
2025-11-28 23:21:58 +0300

    Time: 27 ms (22.04%) | Memory: 17.8 MB (52.31%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,13 +3,13 @@ class Solution:
         counter = defaultdict(int)
     
         left = 0
-        answer = 0
+        result = 0
         
         for right in range(len(word)):
             while counter[word[right]] > 0:
                 counter[word[left]] -= 1
                 left += 1
             counter[word[right]] += 1
-            answer = max(answer, right - left + 1)
+            result = max(result, right - left + 1)
         
-        return answer
+        return result
