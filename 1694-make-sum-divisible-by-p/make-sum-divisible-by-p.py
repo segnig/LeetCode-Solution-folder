5c43f6a36364e8880a2ea52ee7d725cb3d54d812
Segni Girma <segnigirma11@gmail.com>
2025-11-30 18:19:38 +0300

    Time: 102 ms (8.95%) | Memory: 37.8 MB (99.54%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -13,15 +13,15 @@ class Solution:
         mod_map = {
             0: -1
         }
-        current_sum = 0
+        current = 0
         min_len = n
 
     
         for i in range(n):
-            current_sum = (current_sum + nums[i]) % p
-            needed = (current_sum - target + p) % p
+            current = (current + nums[i]) % p
+            needed = (current - target + p) % p
             if needed in mod_map:
                 min_len = min(min_len, i - mod_map[needed])
-            mod_map[current_sum] = i
+            mod_map[current] = i
             
         return -1 if min_len == n else min_len
